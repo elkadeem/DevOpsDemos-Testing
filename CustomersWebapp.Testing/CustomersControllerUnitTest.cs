672974174d7a6be1fec9cd2baf77baf7d93f835c
Wael Elkadim <elkadeem@hotmail.com>
2022-03-22 11:33:24 +0000

    Add unit test

--- CustomersWebapp.Testing/CustomersControllerUnitTest.cs
+++ CustomersWebapp.Testing/CustomersControllerUnitTest.cs
@@ -2,6 +2,7 @@ using CustomersWebapp.Controllers;
 using CustomersWebapp.Models;
 using CustomersWebapp.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
@@ -10,11 +11,12 @@ namespace CustomersWebapp.Testing
 {
     public class CustomersControllerUnitTest
     {
+        private Mock<ICustomersRepository> repositoryStub;
         private readonly CustomersController customersController;
         public CustomersControllerUnitTest()
         {
-            
-            customersController = new CustomersController(new FakeCustomerRepository());
+            repositoryStub = new Mock<ICustomersRepository>();
+            customersController = new CustomersController(repositoryStub.Object);
         }
 
         [Fact]
@@ -33,7 +35,9 @@ namespace CustomersWebapp.Testing
         public async Task Index_Search_By_Name_Will_Return_View()
         {
             //Arrange
-            
+            repositoryStub.Setup(c => c.Get())
+                .Returns(Task.FromResult(new List<Customer>()));
+
             string search = "name1";
             //Act
             var result = await customersController.Index(new CustomersViewModel() { Name = search });
@@ -47,7 +51,9 @@ namespace CustomersWebapp.Testing
         public async Task Index_Search_By_Email_Will_Return_View()
         {
             //Arrange
-            
+            repositoryStub.Setup(c => c.Get())
+                .Returns(Task.FromResult(new List<Customer>()));
+
             string search = "email 1";
             //Act
             var result = await customersController.Index(new CustomersViewModel() { Email = search });
@@ -61,7 +67,9 @@ namespace CustomersWebapp.Testing
         public async Task Create_With_Invalid_Data_Will_Return_To_View_With_Model()
         {
             //Arrange
-            
+            repositoryStub.Setup(c => c.Get())
+                .Returns(Task.FromResult(new List<Customer>()));
+
             var customerModel = new CreateCustomerModel
             {
                 Name = "name",
@@ -81,7 +89,9 @@ namespace CustomersWebapp.Testing
         public async Task Create_With_valid_Data_Will_Return_Redirect_To_Index()
         {
             //Arrange
-            
+            repositoryStub.Setup(c => c.Add(It.IsAny<Customer>()))
+                .ReturnsAsync(true);
+
             var customerModel = new CreateCustomerModel
             {
                 Name = "name",
@@ -94,6 +104,34 @@ namespace CustomersWebapp.Testing
             //Assert
             Assert.IsType<RedirectToActionResult>(result);
             Assert.Equal("Index", ((RedirectToActionResult)result).ActionName);
+        }
+
+        [Fact]
+        public async Task Create_Will_Return_To_View_With_Error_When_Errors_Wile_Saving()
+        {
+            //Arrange
+            repositoryStub.Setup(c => c.Add(It.IsAny<Customer>()))
+                .ReturnsAsync(false);
+
+            var customerModel = new CreateCustomerModel
+            {
+                Name = "name",
+                Email = "elkadeem@hotmail.com",
+                Phone = "21"
+            };
+            //Act
+            var result = await customersController.Create(customerModel);
+
+            //Assert
+            //Here I am working with mock
+            repositoryStub.Verify(c => c.Add(It.IsAny<Customer>()), Times.Once);
+
+            Assert.IsType<ViewResult>(result);
+            Assert.Equal(customerModel, ((ViewResult)result).Model);
+            Assert.False(customersController.ModelState.IsValid);
+            Assert.NotEmpty(customersController.ModelState.Values);
+
+
         }
         #endregion
     }
