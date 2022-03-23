a80c7f3d5f4a88fefea838ca5a5c04baaa2df0e6
Wael Elkadim <elkadeem@hotmail.com>
2022-03-23 06:09:26 +0000

    Add database scripts

--- CustomersWebapp.UITesting/LoginTest.cs
+++ CustomersWebapp.UITesting/LoginTest.cs
@@ -1,5 +1,6 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
@@ -25,7 +26,31 @@ namespace CustomersWebapp.UITesting
         [Fact]
         public void Login_With_UserName_And_Password()
         {
-            _webDriver.Navigate().GoToUrl("");
+            _webDriver.Navigate().GoToUrl("http://20.224.244.228:8081/");
+            var loginLink = _webDriver.FindElement(By.LinkText("Login"));
+            loginLink.Click();
+
+            WebDriverWait wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(10));
+            var emailEelement = wait.Until(c => c.FindElement(By.Name("Input.Email")));
+            var passwordEelement = _webDriver.FindElement(By.Name("Input.Password"));
+
+            emailEelement.SendKeys("elkadeem@hotmail.com");
+            passwordEelement.SendKeys("anypassword");
+
+            var submitButton = _webDriver.FindElement(By.CssSelector("#login-submit"));
+            submitButton.Click();
+
+            emailEelement = wait.Until(c => c.FindElement(By.Name("Input.Email")));
+            if (emailEelement == null)
+                CreateScreenShoot();
+
+            Assert.NotNull(emailEelement);
+        }
+
+        private void CreateScreenShoot()
+        {
+            Screenshot screenshot = (_webDriver as ITakesScreenshot).GetScreenshot();
+            screenshot.SaveAsFile("screenshot.png", ScreenshotImageFormat.Png);
         }
     }
 }
