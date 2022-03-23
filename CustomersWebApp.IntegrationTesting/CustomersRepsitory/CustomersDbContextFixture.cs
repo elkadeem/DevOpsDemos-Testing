e5722eb30f49329c275fca9d804ab59e492261fb
Wael Elkadim <elkadeem@hotmail.com>
2022-03-23 06:49:56 +0000

    Add class fixture to initalize in memeory database

---
+++ CustomersWebApp.IntegrationTesting/CustomersRepsitory/CustomersDbContextFixture.cs
@@ -0,0 +1,38 @@
+﻿using CustomersWebapp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomersWebApp.IntegrationTesting.CustomersRepsitory
+{
+    public class CustomersDbContextFixture : IDisposable
+    {
+        public CustomersDbContext CustomersDbContext { get;}
+        public CustomersDbContextFixture()
+        {
+            DbContextOptionsBuilder<CustomersDbContext> dbContextOptionsBuilder
+                = new DbContextOptionsBuilder<CustomersDbContext>();
+            dbContextOptionsBuilder.UseInMemoryDatabase("testdb");
+            CustomersDbContext = new CustomersDbContext(dbContextOptionsBuilder.Options);
+            CustomersDbContext.Database.EnsureCreated();
+
+            CustomersDbContext.Customers.Add(new Customer("Customer", "email@test.com", "232332")
+            {
+                Department = "dfgsdgsdgdsgd",
+            });            
+        }
+
+        public Task InitalizeDb()
+        {
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            CustomersDbContext.Dispose();
+        }
+    }
+}
--- CustomersWebapp/Repositories/ICustomersRepository.cs
+++ CustomersWebapp/Repositories/ICustomersRepository.cs
@@ -1,5 +1,4 @@
 ﻿using CustomersWebapp.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace CustomersWebapp.Repositories
 {
@@ -8,29 +7,5 @@ namespace CustomersWebapp.Repositories
         Task<List<Customer>> Get();
 
         Task<bool> Add(Customer customer);
-    }
-
-    public class CustomersRepository : ICustomersRepository
-    {
-        private readonly CustomersDbContext customersDbContext;
-
-        public CustomersRepository(CustomersDbContext customersDbContext)
-        {
-            this.customersDbContext = customersDbContext;
-        }
-        public async Task<bool> Add(Customer customer)
-        {
-           await customersDbContext.Customers
-                .AddAsync(customer);
-
-            var result = await customersDbContext.SaveChangesAsync();
-            return result > 0;
-        }
-
-        public Task<List<Customer>> Get()
-        {
-            return customersDbContext.Customers
-                .ToListAsync();
-        }
     }
 }
